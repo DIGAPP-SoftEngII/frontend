b7c6976be11d444c858c75960e6622fb8e90124f
jatoroe <jatoroe@unal.edu.co>
2022-10-11 19:29:59 -0500

    Add statistics feature for Establisment page

---
+++ src/Pages/Establishment/Statistics/Statistics.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Statistics.css";
+
+function Statistics({ est }) {
+  return (
+    <div className="statis">
+      stic
+      <div className="stat__item">
+        <h3>Calificación</h3>
+        <span>{est?.rating}</span>
+      </div>
+      <div className="stat__item">
+        <h3>Internet</h3>
+        <span>{est?.internet_quality}</span>
+      </div>
+      <div className="stat__item">
+        <h3>Capacidad</h3>
+        <span>{est?.capacity}</span>
+      </div>
+    </div>
+  );
+}
+
+export default Statistics;
